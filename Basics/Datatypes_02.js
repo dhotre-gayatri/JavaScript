58960a8638230faf83a81ec5e51d6ac7a8f5f3a1
Gayatri Dhotre <dhotregayatri28@gmail.com>
2025-01-22 08:15:54 +0000

    container config done

---
+++ Basics/Datatypes_02.js
@@ -0,0 +1,1 @@
+"use strict"; //----> we use this to treat all js code as newer version of js
---
+++ Basics/Variables_01.js
@@ -0,0 +1,21 @@
+const account_id = 246
+let account_email = "Dhotre@gmail.com"
+var account_password = "456001" //---> mostly we don't use var because it not respect scope concept
+account_city = "Kolhapur" //----> we can declare variables without using const and let but it is not good practice so it is not preferable
+let accoutn_state
+// account_id = 280........we have declared account_id as const means that we cannot change it
+console.log(account_id );
+
+account_email = "Gayatri@gamail.com"
+console.log(account_email);
+
+account_password = "456280"
+console.log(account_password);
+
+account_city = "Pune"
+console.log(account_city);
+
+console.table([account_id,account_email,account_password,account_city,accoutn_state]); // console.table([]);---> gives output in table format
+
+
+
