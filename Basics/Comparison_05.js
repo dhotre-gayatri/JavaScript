fe422d79c07e2db3000062e305007f9a15569859
Gayatri Dhotre <dhotregayatri28@gmail.com>
2025-01-22 14:49:20 +0000

    comparison

---
+++ Basics/Comparison_05.js
@@ -0,0 +1,21 @@
+/*
+Automatically js has converted 2 and 02 converted into number which are string 
+console.log("2" > 1); 
+console.log("02" > 1);
+*/
+
+
+/*
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+*/
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined > 0);
+
+// ===
+console.log("2"===2);
+
+
