a3dd9dc475884c63818d9dae6086aa7587898b40
Gayatri Dhotre <dhotregayatri28@gmail.com>
2025-01-22 11:07:05 +0000

    conversion operation

---
+++ Basics/Conversion_Operation_03.js
@@ -0,0 +1,34 @@
+let score = " 123gayatri"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let value_number = Number(score)
+console.log(value_number);
+console.log(typeof(value_number))
+
+//"33" = 33
+// "123gayatri" = NaN
+// true = 1
+// false = 0
+
+
+let is_logged_in = true
+
+let boolean_is_loged_in = Boolean(is_logged_in)
+
+console.log(boolean_is_loged_in);
+console.log(typeof(boolean_is_loged_in));
+
+// 1 = true;
+// 0 = flase
+// " gayatri " = true
+// " " = false
+
+let some = 33
+let string_number = String(some)
+console.log(string_number);
+console.log(typeof(string_number));
+
+
+
---
+++ Basics/Conversion_Operation_04.js
@@ -0,0 +1,23 @@
+/*let value = 3
+let negative_value = -value
+console.log(negative_value);*/
+
+
+/*
+let str1 = "Hello"
+let str2 = "gayatri"
+
+console.log(str1+ " " + str2);*/
+
+
+/*console.log("1"+2);
+console.log(2 + "1");
+console.log("1"+ 2+2);
+console.log(1+2+"2");*/
+
+console.log(+true);
+console.log(+"");
+
+
+
+
