63fd471f9d1c4439c72611af9b6a68308ff689a9
Gayatri Dhotre <dhotregayatri28@gmail.com>
2025-01-22 08:44:23 +0000

    datatypes

--- Basics/Datatypes_02.js
+++ Basics/Datatypes_02.js
@@ -1,1 +1,15 @@
 "use strict"; //----> we use this to treat all js code as newer version of js
+
+let name = " Gayatri" // string type of data
+let age = 21 // int type of data
+let is_logged_in = false // boolean type of data
+ // null = standalone value
+ // undefined = value is not assigned
+ // symbol = uniqueness
+
+ //object
+console.log(typeof " Gayatri ");
+
+console.log(typeof age);
+
+console.log(typeof null);
