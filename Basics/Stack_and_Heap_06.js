5f7009992be956009ba447235997601d63b536f7
Gayatri Dhotre <dhotregayatri28@gmail.com>
2025-01-22 16:29:20 +0000

    stack and heap

---
+++ Basics/Stack_and_Heap_06.js
@@ -0,0 +1,21 @@
+// Stack (Peimitive), Heap (Non-Primitive)
+
+let channel = "gayatridhotre28"
+let channel1 = channel
+channel1 = " vidyadhotre29"
+
+console.log(channel);
+console.log(channel1);
+
+
+let dictionary = {
+    email : "gayatridhotre",
+    age : 21
+}
+let dictionary1 = dictionary
+dictionary1.email = "vidyadhotre29"
+console.log();
+console.log(dictionary);
+console.log(dictionary1);
+
+
