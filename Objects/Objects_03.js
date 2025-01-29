fcc8698c6589da07db38a1d6e83be591dc0c22bb
Gayatri Dhotre <dhotregayatri28@gmail.com>
2025-01-29 05:29:31 +0000

    objects

---
+++ Objects/Objects_03.js
@@ -0,0 +1,27 @@
+const course = {
+    name: "Gayatri",
+    email : "gayatridhotre28@gmail.com",
+}
+// objects destructuring
+const {email} = course
+console.log(email);
+
+
+/*
+// JSON api doesn't have name just give curly braces {}
+// even if it is showing error at : but just ignore it the syntax of json api is perfect
+{
+  name : "Gayatri",
+  course_name : "JavaScript in Hindi",
+  price : "free"
+}
+  */
+
+// we may get api in the form of array
+[
+    {},
+    
+    {},
+
+    {}
+]
